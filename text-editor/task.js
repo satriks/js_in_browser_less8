479ef6acc302b7f742aeb7660e763657a01eac8d
Yury Vereskun <satriks@mail.ru>
2023-07-03 11:22:08 +0300

    done text-editor

---
+++ text-editor/task.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', getDataFromStorage)
+document.querySelector('#editor').addEventListener('input', handle)
+document.querySelector('button').addEventListener('click', cleaner)
+
+function handle(event) {
+    localStorage.setItem('data', this.value)
+}
+
+function getDataFromStorage() {
+   document.querySelector('#editor').value = localStorage.getItem('data')
+}
+
+function cleaner() {
+    document.querySelector('#editor').value =''
+    localStorage.setItem('data', "")
+    
+}
